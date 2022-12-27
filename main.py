fdb0d85d11cc47d87e6c57eb288bde53fb2e4bf0
sumukh-kumar <dev.sumukhkumar@gmail.com>
2022-12-27 20:06:28 +0530

    Changes to the main menu temporarily

--- main.py
+++ main.py
@@ -4,18 +4,40 @@ import images
 
 root = Tk()
 root.title("Modern Art Generator")
-root.geometry("1280x720")
+root.geometry("1920x1080")
 
 
 
-bg= PhotoImage(file="images\Blank_BG.png")
+bg= PhotoImage(file="images\Temp_BG.png")
 
 
-#my_canvas = Canvas(root,width=800,height=500)
+my_canvas = Canvas(root)
+my_canvas.pack(fill="both",expand=True)
+
+my_canvas.create_image(0,0,image=bg,anchor="nw")
+
+my_canvas.create_text(400,250,text="Modern Art Generator",font=("",20),fill="White")
+
+
+button1= Button(root,text="Start")
+button2= Button(root,text="Options")
+button3= Button(root,text="Exit")
+
+
+button1_window = my_canvas.create_window(50,500,anchor="nw",window=button1)
+button2_window = my_canvas.create_window(50,650,anchor="nw",window=button2)
+button3_window = my_canvas.create_window(50,800,anchor="nw",window=button3)
 
 
 
 
+
+
+
+
+
+
+""" 
 my_label = Label(root,image=bg)
 my_label.place(x=0,y=0,relwidth=1,relheight=1)
 
@@ -27,7 +49,7 @@ button3= Button(root,text="Exit")
 fake_text.grid(row=0,column=0,padx=20,pady=100)
 button1.grid(row=1,column=0,padx=20,pady=50)
 button2.grid(row=2,column=0,padx=20,pady=50)
-button3.grid(row=3,column=0,padx=20,pady=50)
+button3.grid(row=3,column=0,padx=20,pady=50) """
 
 
 root.mainloop()
